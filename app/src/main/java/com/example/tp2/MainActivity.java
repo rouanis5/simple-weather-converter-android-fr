3bbb6dc75c3c74ebb224f975f7af5503699ccc76
rouanis5 <ouanis42@gmail.com>
2022-11-11 18:42:14 +0100

    deal with main click events

--- app/src/main/java/com/example/tp2/MainActivity.java
+++ app/src/main/java/com/example/tp2/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.tp2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        setTitle("Exercice 4");
+        setTitle("Convertisseur de température");
+
+        Button cBtn = findViewById(R.id.celciusBtn);
+        cBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchTo("C");
+            }
+        });
+
+        Button fBtn = findViewById(R.id.fahrenheitBtn);
+        fBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchTo("F");
+            }
+        });
+
+        Button kBtn = findViewById(R.id.kelvinBtn);
+        kBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switchTo("K");
+            }
+        });
     }
+
+    public void switchTo(String type) {
+        Bundle bundle = new Bundle();
+        bundle.putString("type", type);
+
+        Intent intent = new Intent(this, Celisius.class);
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
+
 }
